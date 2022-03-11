65362c7d969daf0835b830c59a39906fc22a31bf
moatadil <moatadil.2013@gmail.com>
2022-03-11 14:18:45 +0200

    dummy

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import './App.css';
 import {useState} from 'react'
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/NewExpense/NewExpense'
-const dummyData = [
-    { 
-      amount:2520,
-      title: 'Car Insurance',
-      date: new Date(2022,2,10),
-      id : 1
-    }
-  ]
 const App = () => {
   const [data, setData] = useState([])
   const saveNewData = newData => {
