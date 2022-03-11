7b0105e52a899737d8828135727a22554b4bd4cf
moatadil <moatadil.2013@gmail.com>
2022-03-11 14:04:29 +0200

    test

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -5,7 +5,7 @@ import './ExpensesList.css';
 
 const ExpensesList = (props) => {
   if (props.items.length === 0) {
-    return <h2 className='expenses-list__fallback'>Found no expenses.</h2>;
+    return <h2 className='expenses-list__fallback'>No Expenses Found .</h2>;
   }
 
   return (
