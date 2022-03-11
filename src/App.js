165d4e1e599ce3fcd6bddf4f1bfd09cf127a1a7e
moatadil <moatadil.2013@gmail.com>
2022-03-11 14:34:22 +0200

    change title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react'
+import { useState } from 'react'
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/NewExpense/NewExpense'
 const App = () => {
